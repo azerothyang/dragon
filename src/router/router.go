e0a3b26d06f70a27d2f3eb4febd28f835f92f6b2
azerothYang <azerothyangg@gmail.com>
2018-11-02 16:03:50 +0800

    add custom not found handler

--- src/router/router.go
+++ src/router/router.go
@@ -5,17 +5,31 @@ package router
  */
 import (
 	"ctrl"
+	"fmt"
 	"github.com/julienschmidt/httprouter"
+	"net/http"
 )
+
+type notFoundHandler struct {
+}
 
 var (
 	Routes *httprouter.Router
+	testCtrl = &ctrl.Test{}  //test controller
 )
 
 func init() {
 	Routes = httprouter.New()
-	Routes.GET("/", (&ctrl.Test{}).Test)
-	Routes.POST("/upload", (&ctrl.Test{}).Upload)
-	Routes.GET("/db", (&ctrl.Test{}).GetDBData)
-	Routes.GET("/redis", (&ctrl.Test{}).GetRedis)
+	Routes.GET("/", testCtrl.Test)
+	Routes.POST("/upload", testCtrl.Upload)
+	Routes.GET("/db", testCtrl.GetDBData)
+	Routes.GET("/redis", testCtrl.GetRedis)
+	Routes.NotFound = notFoundHandler{}
+}
+
+// not found route handle
+func (notFoundHandler)ServeHTTP(w http.ResponseWriter, r *http.Request)  {
+	w.Header().Set("content-type", "text/html; charset=utf-8")
+	fmt.Fprintf(w, "<h2>Not Found</h2>")
+	//baseCtrl.Json("not found", w)
 }
